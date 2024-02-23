0c2defe31b4dfd46208a7259c8a26af5f8cba751
Hector-dvi <h.dvi.sg@gmail.com>
2024-02-23 13:33:17 +0000

    first code base

---
+++ base_game.py
@@ -0,0 +1,17 @@
+from pettingzoo.atari import warlords_v3
+#import matplotlib.pyplot as plt
+
+env = warlords_v3.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+
+    env.step(action)
+env.close()
