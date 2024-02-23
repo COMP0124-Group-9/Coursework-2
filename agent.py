8a5c16cfb78a9e44d7e9a42d5313f9c1ba5b09ad
Hector-dvi <h.dvi.sg@gmail.com>
2024-02-23 13:36:41 +0000

    first code base

---
+++ agent.py
@@ -0,0 +1,6 @@
+class Agent():
+
+    def __init__(self):
+        self.win_number = 0
+        self.position = 0
+        pass
--- base_game.py
+++ base_game.py
@@ -1,17 +1,48 @@
 from pettingzoo.atari import warlords_v3
 #import matplotlib.pyplot as plt
+import agent
+import typing
 
-env = warlords_v3.env(render_mode="human")
-env.reset(seed=42)
+class Game:
 
-for agent in env.agent_iter():
-    observation, reward, termination, truncation, info = env.last()
+    def __init__(self,agent_list:typing.List[agent.Agent]):
 
-    if termination or truncation:
-        action = None
-    else:
-        # this is where you would insert your policy
-        action = env.action_space(agent).sample()
+        self.env = warlords_v3.env(render_mode="human")
+        self.env.reset(seed=42)
+        self.agent_list = agent_list
+        self.number_rounds = 0
 
-    env.step(action)
-env.close()
+    def reset(self,seed):
+        self.env.reset(seed=seed)
+
+    def getPlayerArea(observation,agent):
+        # crop to game player region, reflect so player is top left
+        raise NotImplementedError()
+
+    def parseObservation(self,observation):
+        player_area = getPlayerArea(observation,agent)
+        return observation
+    
+    def findBall(self, player_area):
+        pass
+
+
+    def run(self):
+
+        for agent in self.env.agent_iter():
+            observation, reward, termination, truncation, info = self.env.last()
+            parsed_observation = self.parseObservation(observation)
+
+            if termination or truncation:
+                action = None
+            else:
+                # this is where you would insert your policy
+                action = self.env.action_space(agent).sample()
+
+            self.env.step(action)
+    
+    def exit(self):
+        self.env.close()
+
+if __name__ == "__main__":
+    pass
