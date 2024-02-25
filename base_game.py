8f368db77e119ec06727f307311144beb32c039e
Joe <30624504+joe-down@users.noreply.github.com>
2024-02-25 20:59:43 +0000

    Fixes to make runnable

--- base_game.py
+++ base_game.py
@@ -1,49 +1,98 @@
-from pettingzoo.atari import warlords_v3
-#import matplotlib.pyplot as plt
-import agent
 import typing
+from typing import Tuple
+
+import numpy as np
+from pettingzoo.atari import warlords_v3
+
+from Agent import Agent
+
 
 class Game:
-
-    def __init__(self,agent_list:typing.List[agent.Agent]):
-
-        self.env = warlords_v3.env(render_mode="human")
-        self.env.reset(seed=42)
+    def __init__(self, agent_list: typing.List[Agent]) -> None:
         self.agent_list = agent_list
         self.number_rounds = 0
 
-    def reset(self,seed):
-        self.env.reset(seed=seed)
+    @staticmethod
+    def get_player_areas(observation: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        cropped_area = observation[16:-28, :, :]
+        play_area_shape = cropped_area.shape
+        assert play_area_shape[0] % 2 == 0
+        assert play_area_shape[1] % 2 == 0
+        quadrant_height, quadrant_width = [play_area_shape[0] // 2, play_area_shape[1] // 2]
+        return (cropped_area[:quadrant_height, :quadrant_width],
+                np.flip(cropped_area[quadrant_height:, :quadrant_width], axis=0),
+                np.flip(cropped_area[:quadrant_height, quadrant_width:], axis=1),
+                np.flip(cropped_area[quadrant_height:, quadrant_width:], axis=(0, 1)))
 
-    def getPlayerArea(self,observation,agent):
-        # crop to game player region, reflect so player is top left
-        raise NotImplementedError()
+    @staticmethod
+    def block_statuses(player_area: np.ndarray) -> np.ndarray:
+        # TODO can be simplified per block, not per pixel
+        segment_1 = player_area[16:40, 0:28, :].reshape(-1, 3)
+        segment_2 = player_area[0:40, 28:48, :].reshape(-1, 3)
+        return np.sign(np.concatenate((segment_1, segment_2)).sum(axis=-1))
 
-    def parseObservation(self,observation):
-        return observation
-        player_area = self.getPlayerArea(observation,agent)
-        return observation
-    
-    def findBall(self, player_area):
-        pass
+    @staticmethod
+    def base_status(player_area: np.ndarray) -> np.ndarray:
+        # Base is destroyed if all black pixels. True if base exists, false otherwise
+        base = player_area[0:16, 0:28, :]
+        return np.array([np.any(base)])
 
+    @staticmethod
+    def ball_boundary(player_area: np.ndarray) -> np.ndarray:
+        return np.array([0, 0, 0, 0])
+
+    @staticmethod
+    def paddle_boundary(player_area: np.ndarray) -> np.ndarray:
+        return np.array([0, 0, 0, 0])
+
+    def parse_observation(self, observation: np.ndarray, agent_id: str):
+        player_areas = self.get_player_areas(observation)
+        player_statuses = []
+        for player_area in player_areas:
+            base_status = self.base_status(player_area)
+            ball_boundary = self.ball_boundary(player_area)
+            paddle_boundary = self.ball_boundary(player_area)
+            block_status = self.block_statuses(player_area)
+            player_statuses.append(np.concatenate((base_status, ball_boundary, paddle_boundary, block_status)))
+        # TODO correct ordering of these
+        if agent_id == "first_0":
+            new_observation = np.concatenate(player_statuses)
+        elif agent_id == "second_0":
+            new_observation = np.concatenate(player_statuses)
+        elif agent_id == "third_0":
+            new_observation = np.concatenate(player_statuses)
+        else:
+            new_observation = np.concatenate(player_statuses)
+        return new_observation
+
+    def get_agent_dict(self, env):
+        return {agent_id: agent for agent_id, agent in zip(env.agents, self.agent_list)}
+
+    def get_action(self, agent_dict, agent, observation, info):
+        return agent_dict[agent].action(observation=self.parse_observation(observation, agent), info=info)
 
     def run(self):
-
-        for agent in self.env.agent_iter():
-            observation, reward, termination, truncation, info = self.env.last()
-            parsed_observation = self.parseObservation(observation)
-
+        env = warlords_v3.env(render_mode="human")
+        env.reset(seed=42)
+        agent_dict = self.get_agent_dict(env=env)
+        for agent in env.agent_iter():
+            observation, reward, termination, truncation, info = env.last()
             if termination or truncation:
                 action = None
             else:
-                # this is where you would insert your policy
-                action = self.env.action_space(agent).sample()
+                action = self.get_action(agent_dict=agent_dict, agent=agent, observation=observation, info=info)
+            env.step(action)
+        env.close()
 
-            self.env.step(action)
-    
-    def exit(self):
-        self.env.close()
-
-if __name__ == "__main__":
-    pass
+    def run_parallel(self):
+        env = warlords_v3.parallel_env(render_mode="human")
+        observations, infos = env.reset()
+        agent_dict = self.get_agent_dict(env=env)
+        while env.agents:
+            actions = {agent: self.get_action(agent_dict=agent_dict,
+                                              agent=agent,
+                                              observation=observations[agent],
+                                              info=infos[agent])
+                       for agent in env.agents}
+            observations, rewards, terminations, truncations, infos = env.step(actions)
+        env.close()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import agent
-import base_game
+from Agent import Agent
+from base_game import Game
 
 
 def main():
-    agents = [agent.Agent() for _ in range(4)]
-    game = base_game.Game(agent_list=agents)
-    game.run()
+    agents = [Agent() for _ in range(4)]
+    game = Game(agent_list=agents)
+    game.run_parallel()
 
 
 if __name__ == '__main__':
