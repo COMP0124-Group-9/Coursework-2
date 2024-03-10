032072679e6c633ef1821e8414e7bfe8adb7194b
Joe <30624504+joe-down@users.noreply.github.com>
2024-03-10 16:15:29 +0000

    Main loop tweak

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from Game import Game
 def main():
     agents = [Agent() for _ in range(4)]
     game = Game(agent_list=agents)
-    game.run_parallel()
+    game_count: int = 0
+    while True:
+        print(f"Starting Game {game_count}")
+        game.run_parallel()
+        game_count += 1
 
 
 if __name__ == '__main__':
