e63d069ca3e2c40ea7d811cbb5b16b43b57003c2
Joe <30624504+joe-down@users.noreply.github.com>
2024-03-13 20:17:00 +0000

    Export model on exit, load on next start

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import datetime
+import pathlib
 import time
 from typing import Tuple
 
 import numpy as np
+import torch
 
 from Agent import Agent
 from Game import Game, EXPECTED_OBSERVATION_LENGTH, BLOCKS_PER_PLAYER, BALL_COORDINATE_SHAPE
@@ -24,14 +27,30 @@ def generate_reward_vector(base_status_weights: Tuple[float, float, float, float
 
 def main():
     agents = [Agent(reward_vector=generate_reward_vector()) for _ in range(4)]
-    game = Game(agent_list=agents)
-    game_count: int = 0
-    while True:
-        start_time = time.time()
-        print(f"Starting Game {game_count}")
-        game.run_parallel()
-        game_count += 1
-        print(f"finished in {time.time() - start_time} seconds")
+    model_path = pathlib.Path("Models")
+    if model_path.exists():
+        runs = [run.stem for run in model_path.iterdir()]
+        if len(runs) > 0:
+            states_root = model_path / max(runs)
+            print(f"Loaded from {states_root}")
+            for agent_number, agent in enumerate(agents):
+                agent.model.load_state_dict(torch.load(states_root / f"{agent_number}.pt"))
+    try:
+        game = Game(agent_list=agents)
+        game_count: int = 0
+        while True:
+            start_time = time.time()
+            print(f"Starting Game {game_count}")
+            game.run_parallel()
+            game_count += 1
+            print(f"finished in {time.time() - start_time} seconds")
+    except KeyboardInterrupt:
+        path = model_path / datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        if not path.parent.exists():
+            path.parent.mkdir()
+        path.mkdir()
+        for agent_number, agent in enumerate(agents):
+            torch.save(agent.model.state_dict(), path / f"{agent_number}.pt")
 
 
 if __name__ == '__main__':
