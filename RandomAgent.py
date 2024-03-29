c719852f59c5b2d2618e0f1245ceaad6fbc1e198
Joe Down <joe-down@users.noreply.github.com>
2024-03-29 03:19:06 +0000

    Random agent fix

--- RandomAgent.py
+++ RandomAgent.py
@@ -4,5 +4,5 @@ import TrainedAgent
 
 
 class RandomAgent(TrainedAgent.TrainedAgent):
-    def action(self, observation, info):
+    def action(self, observation):
         return numpy.random.choice(self.possible_actions)
